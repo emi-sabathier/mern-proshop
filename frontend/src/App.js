cdb4fd43b7c4f16bad32e3b81d6c5640064f9566
emi-sabathier <sabathier.emilie@gmail.com>
2020-11-22 08:58:10 +0100

    first push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Container} from 'react-bootstrap'
 
 const App = () => {
     return (
         <>
-            <h1>Bienvenue</h1>
+            <Header/>
+            <Container>
+                <h1>Bienvenue</h1>
+            </Container>
+            <Footer/>
         </>
     );
 }
