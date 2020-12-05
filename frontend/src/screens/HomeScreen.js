6e4be4c7bd1fc194b8388c1750aafe2d9e9a92eb
Emi <sabathier.emilie@gmail.com>
2020-12-05 14:49:07 +0100

    fetching products list / product from api - adding proxy

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import products from '../products'
+import React, {useState, useEffect} from 'react'
 import Product from '../components/Product'
-import {Row, Col} from "react-bootstrap";
+import {Row, Col} from "react-bootstrap"
+import axios from "axios"
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const {data} = await axios.get('/api/products')
+            setProducts(data)
+        }
+        fetchProducts()
+    }, [])
     return (
         <>
             <h1>Derniers produits</h1>
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={3} xl={3}>
-                        <Product product={product} />
+                        <Product product={product}/>
                     </Col>
                 ))}
             </Row>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from "axios";
 
 const ProductScreen = ({match}) => {
-    /* tu récuperes l'id en get depuis l'url
-et tu compares a ta bdd pour te sortir l'article qui match l'id
-genre si ta un id 964537876
-et que ta pas cet id la dans un de tes produits ca va te retourner une page blanche
-     */
-    const product = products.find(p => p._id === match.params.id)
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`/api/products/${match.params.id}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [])
+
     return (
         <>
             <Link className="btn btn-light my-3" to='/'>
