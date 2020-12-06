330e62a16212c8a20870d164a35407ca1232239e
Emi <sabathier.emilie@gmail.com>
2020-12-06 18:34:45 +0100

    Adding nodemon, env

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const products = require('./data/products')
+// const express = require('express')
+// const dotenv = require('dotenv')
+// const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('API is running')
+    res.send('API ouvert')
 })
 
 app.get('/api/products', (req, res) => {
@@ -13,7 +19,7 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find(p => p._id === req.params.id)
     res.json(product)
 })
-
-app.listen(5000, console.log('server running on 5000'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on ${PORT}`))
 
 
